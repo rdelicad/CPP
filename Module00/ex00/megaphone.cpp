b51705dfc49d41b8fc3dd7afb4f6b28004f52233
rdelicad <rdelicad@student.42malaga.com>
2024-02-12 11:31:57 +0100

    ex00

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -1,9 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/12 10:21:15 by rdelicad          #+#    #+#             */
+/*   Updated: 2024/02/12 10:40:45 by rdelicad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	int a, b;
-	std::cin >> a >> b;
-	std::cout << a + b;
+	if (ac > 1)
+	{
+		int i = 1;
+
+		while (av[i])
+		{
+			int j = 0;
+			while (av[i][j])
+			{
+				if (av[i][j] >= 'a' && av[i][j] <= 'z')
+					av[i][j] -= 32;
+				std::cout << av[i][j];
+				j++;
+			}
+			i++;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	return (0);
 }
