7a6a4d846c91825d0a3f8b4bab0563e536dbb288
rdelicad <rdelicad@student.42malaga.com>
2024-02-15 23:38:47 +0100

    agregando contacto

--- Module00/ex01/src/options.cpp
+++ Module00/ex01/src/options.cpp
@@ -6,7 +6,7 @@
 /*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 19:16:34 by rdelicad          #+#    #+#             */
-/*   Updated: 2024/02/15 20:56:05 by rdelicad         ###   ########.fr       */
+/*   Updated: 2024/02/15 23:37:25 by rdelicad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,39 @@ void	addContact()
 {
 	t_agenda 	a;
 	Contact		agenda;
+	int			i;
+	std::string input;
+	std::string	array[] = {"name", "last", "nick", "phone", "secret"};
+
+	i = 0;
+	do
+	{
+		std::cout << "add first " << array[i] << std::endl;
+		std::cin >> input;
+		switch(i)
+		{
+			case 0:
+				agenda.setFirstName(input);
+				break;
+			case 1:
+				agenda.setLastName(input);
+				break;
+			case 2:
+				agenda.setNickName(input);
+				break;
+			case 3:
+				agenda.setPhoneNumber(input);
+				break;
+			case 4:
+				agenda.setDarkestSecret(input);
+				break;
+		}
+		i++;
+	} while (i < 5 && input != "");
+	
 
 	//clearConsole();
-	std::cout << std::endl;
+	/* std::cout << std::endl;
 	std::cout << "add first name" << std::endl;
 	std::cin >> a.name;
 	if (a.name != "")
@@ -42,7 +72,7 @@ void	addContact()
 	std::cout << "add darkt secret" << std::endl;
 	std::cin >> a.secret;
 	if (a.secret != "")
-		agenda.setFirstName(a.secret);
+		agenda.setFirstName(a.secret); */
 	
 	std::cout << std::endl;
 	std::cout << "the contact has been added" << std::endl;
