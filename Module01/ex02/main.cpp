75270863d611c1a2331169d008bae55285c77574
rdelicad <rdelicad@student.42malaga.com>
2024-03-07 15:50:41 +0100

    Module01 corregido

--- Module01/ex02/main.cpp
+++ Module01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 14:22:13 by rdelicad          #+#    #+#             */
-/*   Updated: 2024/02/29 16:53:06 by rdelicad         ###   ########.fr       */
+/*   Updated: 2024/03/07 15:15:21 by rdelicad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,6 @@ int	main( void )
 	std::string	string = "HI THIS IS BRAIN";
 	std::string *stringPTR = &string;
 	std::string &stringREF = string;
-	int var = 2;
-	int *ptr;
-	int &ref;
-	
-	ptr = &var;
-	&ref = var;
 	
 	std::cout << "Address of the string variable: "	<< &string		<< std::endl;
 	std::cout << "Address held by stringPTR:      "	<< stringPTR	<< std::endl;
@@ -32,9 +26,6 @@ int	main( void )
 	std::cout << "value of the string variable:   "	<< string		<< std::endl;
 	std::cout << "value pointed to by stringPTR:  "	<< *stringPTR	<< std::endl;
 	std::cout << "value pointed to by stringREF:  "	<< stringREF	<< std::endl;
-	
-	std::cout << ptr << std::endl;
-	std::cout << *ptr << std::endl;
-	
+
 	return 0;
 }
