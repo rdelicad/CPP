4ff931c87b868081ec7bcfe13e7c90e556173a3f
rdelicad <rdelicad@student.42malaga.com>
2024-03-09 12:49:47 +0100

    ex01

---
+++ Module02/ex01/Fixed.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 12:03:24 by rdelicad          #+#    #+#             */
+/*   Updated: 2024/03/09 12:48:02 by rdelicad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fixedPointValue(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( const int intNbr )
+{
+	std::cout << "Int constructor called" << std::endl;
+	_fixedPointValue = intNbr << _fractionalBits;
+}
+
+Fixed::Fixed( const float floatNbr )
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fixedPointValue = static_cast<float>(floatNbr * (1 << _fractionalBits));
+}
+
+Fixed::Fixed( const Fixed& copy )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	_fixedPointValue = copy._fixedPointValue;
+}
+
+Fixed& Fixed::operator = ( const Fixed& other )
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		_fixedPointValue = other.getRawBits();
+	
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits() const
+{
+	return this->_fixedPointValue;
+}
+
+void Fixed::serRawBits( int const raw )
+{
+	this->_fixedPointValue = raw;
+}
+
+int Fixed::toInt() const
+{
+	return this->_fixedPointValue >> _fractionalBits;
+}
+
+float Fixed::toFloat() const
+{
+	return static_cast<float>(this->getRawBits()) / (1 << _fractionalBits);
+}
+
+std::ostream& operator << ( std::ostream& op, Fixed const & i )
+{
+	op << i.toFloat();
+	return op;
+}
--- Module02/ex01/Fixed.hpp
+++ Module02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 19:21:34 by rdelicad          #+#    #+#             */
-/*   Updated: 2024/03/08 18:23:19 by rdelicad         ###   ########.fr       */
+/*   Updated: 2024/03/09 12:35:38 by rdelicad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,19 @@ class Fixed
 		static const int	_fractionalBits = 8;
 	public:
 		Fixed( void );
-		Fixed( const int nbr );
+		Fixed( const int intNbr );
+		Fixed( const float floatNbr );
 		Fixed( const Fixed& copy );
 		Fixed& operator = ( const Fixed& other );
 		~Fixed( void );
+
+		int getRawBits( void ) const;
+		void serRawBits( int const raw );
 		
 		float toFloat( void ) const;
 		int toInt( void ) const;
 };
+
+std::ostream& operator << ( std::ostream& op, Fixed const & i);
 
 #endif
